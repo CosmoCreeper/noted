dee73375b2156a95a486412b8986a86f8c72a834
Solomon Latham <lathamsolomon12@gmail.com>
2024-10-29 13:22:54 -0400

    Recovered git.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'body': 'Roboto',
+      'header': ["Franklin Gothic Medium", "Arial Narrow", 'Arial', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
+
